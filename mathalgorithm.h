86d6e1973347c8d583fec2952dfe002bffa1ecc6
lambdacdm <54066054+lambdacdm@users.noreply.github.com>
2022-09-27 21:48:24 +0800

    Add files via upload

---
+++ mathalgorithm.h
@@ -0,0 +1,20 @@
+// version 0.7.5 pre-release
+#ifndef MATHALGORITHM_H
+#define MATHALGORITHM_H
+#include "numbertheory.h"
+#include "highprecision.h"
+#include "ODE.h"
+#include "probability.h"
+#include "optimization.h"
+#include "plot.h"
+using namespace nbt;
+using namespace hpc;
+using namespace ode;
+using namespace opt;
+using namespace prb;
+using namespace plt;
+
+namespace mal{
+
+}
+#endif
